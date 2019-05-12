950e3892a0f59f2316d6a9de6074393727d65461
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-05-12 17:37:57 -0300

    working writing file

--- ES_DAY/app.py
+++ ES_DAY/app.py
@@ -1,30 +1,33 @@
 # imports
 import methods
-import keyboard
 
+methods.welcomeScreen()
 p = True
 while p:
     matricula = input('MATRICULA: ')
-    matricula = matricula[0:8]
+    if(len(matricula) < 8):
+        print("MATRICULA INVÁLIDA! TENTE NOVAMENTE")
+        continue
+    else:
+        matricula = matricula[0:8]
     print("\n")
-    print("-------------------------------------------------------------------------")
-    print("\n")
+    print("--------------------------------------------------------------------------------------")
     print('MATRICULA DO ALUNO: ',matricula)
     print("\n")
-    check = input('CONFIRMAR MATRICULA \n [ENTER]Confirmar  [t/T]Tentar novamente : ')
-    print("\n")
-    print("-------------------------------------------------------------------------")
-    if(check == 't' or check == 'T'):
-        continue
-    elif(keyboard.is_pressed('enter')):
+    check = input('CONFIRMAR MATRICULA: \n[ENTER]Confirmar  [t/T]Tentar novamente [f/F]Concluir: ')
+    print("--------------------------------------------------------------------------------------")
+    if(check == 'ENTER' or check == ""):
         print('MATRICULA CONFIRMADA!')
         methods.writeOnFile(matricula)
+    elif(check == 't' or check == 'T'):
+        continue
+    elif(check == 'f' or check == 'F'):
+        print('CONFIRMAÇÃO COM ES-DAY CONCLUIDO!')
+        p = False
     else:
         print('Comando Não confirmado, tente novamente!')
-        check
+        continue
 
-# methods.writeOnFile(matricula)
-methods.readOfFile("matriculas.csv")
 
 
 
--- ES_DAY/methods.py
+++ ES_DAY/methods.py
@@ -1,13 +1,23 @@
 def writeOnFile(matricula):
-    file = open("matriculas.csv","w")
-    matricula = matricula + "\n"
+    file = open("matriculas.csv","a+")
+    matricula = f"{matricula}\n"
     file.write(matricula)
     file.close()
     
 def readOfFile(arquivo):
     file = open(arquivo,"r")
     content = file.read()
-    print(cont)
+    print(content)
     file.close()
 
-                
+def welcomeScreen():
+    print('--------------------------------------------------------------------')
+    print('________ _____          ______    __ __     __ ____   ___  ___ ___  ')
+    print('|  ____|/ ____|         |  __ \   /\ \ \   / / |__ \ / _ \/_ |/ _ \ ')
+    print('| |__  | (___   ______  | |  | | /  \ \ \_/ /     ) | | | || | (_) |')
+    print('|  __|  \___ \  ______  | |  | |/ /\ \ \   /     / /| | | || |\__, |')
+    print('| |____ ____) |         | |__| / ____ \ | |     / /_| |_| || |  / / ')
+    print('|______|_____/          |_____/_/    \_ \_|    |____|\___/ |_| /_/  ')
+    print('--------------------------------------------------------------------')
+    print('\n')
+    print('\n')           
